4ccffea7d5187f63ad5a9317b75ee60087d10e66
Jan Sedivy <jansedivy@abdoc.net>
2014-03-07 16:34:51 +0100

    change tabs to 2 spaces

--- app/templates/src/scripts/home/HomeCtrl.js
+++ app/templates/src/scripts/home/HomeCtrl.js
@@ -1,4 +1,4 @@
 angular.module('app.home')
-	.controller('HomeCtrl', function($scope) {
-		$scope.version = '0';
-	});
+  .controller('HomeCtrl', function($scope) {
+    $scope.version = '0';
+  });
