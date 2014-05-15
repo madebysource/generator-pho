af917b1d96d22981fe7e2ad6a0ad13e37fcd3162
Jan Sedivy <jansedivy@abdoc.net>
2014-05-15 15:36:49 +0200

    replace file-insert and inject with substituter

--- app/templates/gulpfile-production.js
+++ app/templates/gulpfile-production.js
@@ -1,6 +1,17 @@
 // For all available options, see node_modules/pho-dev-stack/config.js
 // These are production build settings, see gulpfile.js for development settings
 var gulp = require('gulp');
+var through = require('through2');
+
+// get stream of filenames in directory
+var files = function(path, format) {
+  return gulp.src(path, { read: false })
+    .pipe(through.obj(function(file, enc, callback) {
+      this.push(format(file.relative));
+      callback();
+    }));
+};
+
 require('pho-devstack')(gulp, {
   browserify: {
     debug: false,
@@ -35,7 +46,21 @@ require('pho-devstack')(gulp, {
   base64: {
     enabled: true,
     // baseDir: 'src' // uncomment if you are using absolute paths
-  }
+  },
+
+  substituter: {
+    livereload: function() {
+      return gulp.src('src/partials/livereload.txt');
+    },
+
+    js: files('dist/scripts/**/*.js', function(name) {
+      return '<script src="scripts/' + name + '"></script>';
+    }),
+
+    css: files('dist/styles/**/*.css', function(name) {
+      return '<link rel="stylesheet" href="styles/' + name + '">';
+    })
+  },
 });
 
 // If needed, redefine tasks here
