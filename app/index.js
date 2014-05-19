8350d1ae6745984669fa587d076ffd7ebbe74b2b
Jan Sedivy <jansedivy@abdoc.net>
2014-05-19 12:32:52 +0200

    update to generator 0.17.0 with new syntax

--- app/index.js
+++ app/index.js
@@ -1,154 +1,151 @@
 'use strict';
-var util = require('util');
-var path = require('path');
 var yeoman = require('yeoman-generator');
 
-var SourceGenerator = module.exports = function SourceGenerator(args, options, config) {
-  yeoman.generators.Base.apply(this, arguments);
+module.exports = yeoman.generators.Base.extend({
+  constructor: function(args, options) {
+    yeoman.generators.Base.apply(this, arguments);
 
-  // have Yeoman greet the user.
-  console.log(this.yeoman);
+    console.log(this.yeoman);
 
-  this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
-  });
+    this.pkg = require('../package.json');
 
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
-};
+    this.on('end', function () {
+      this.installDependencies({ skipInstall: options['skip-install'] });
+    });
+  },
 
-util.inherits(SourceGenerator, yeoman.generators.Base);
+  askFor: function() {
+    var cb = this.async();
 
-SourceGenerator.prototype.askFor = function() {
-  var cb = this.async();
-
-  var prompts = [
-    {
-      type: 'list',
-      name: 'type',
-      message: 'type',
-      choices: ['web app', 'landing page']
-    }
-  ];
-
-  this.prompt(prompts, function(props) {
-    this.type = props.type;
-
-    cb();
-  }.bind(this));
-};
-
-SourceGenerator.prototype.askForMetatags = function() {
-  var cb = this.async();
-
-  var prompts = [
-    {
-      type: 'confirm',
-      name: 'metatags',
-      message: 'metatags'
-    },
-    {
-      type: 'confirm',
-      name: 'analytics',
-      message: 'google analytics'
-    }
-  ];
-
-  this.prompt(prompts, function(props) {
-    this.metatags = props.metatags;
-    this.analytics = props.analytics;
-
-    cb();
-  }.bind(this));
-};
-
-SourceGenerator.prototype.askForAngular = function() {
-  var cb = this.async();
-
-  var prompts = [];
-
-  if (this.type === 'web app') {
-    prompts = [
+    var prompts = [
       {
-        type: 'confirm',
-        name: 'angular',
-        message: 'angular'
-      },
-      {
-        type: 'confirm',
-        name: 'tests',
-        message: 'tests',
-        when: function(props) {
-          return props.angular;
-        }
-      },
-      {
-        type: 'confirm',
-        name: 'unit',
-        message: 'unit testing (jasmine)',
-        when: function(props) {
-          return props.tests;
-        }
-      },
-      {
-        type: 'confirm',
-        name: 'e2e',
-        message: 'e2e testing (casper.js)',
-        when: function(props) {
-          return props.tests;
-        }
+        type: 'list',
+        name: 'type',
+        message: 'type',
+        choices: ['web app', 'landing page']
       }
     ];
-  }
 
-  this.prompt(prompts, function(props) {
-    this.angular = props.angular;
-    this.tests = props.tests;
-    this.unit = props.unit;
-    this.e2e = props.e2e;
+    this.prompt(prompts, function(props) {
+      this.type = props.type;
 
-    cb();
-  }.bind(this));
-};
+      cb();
+    }.bind(this));
+  },
 
-SourceGenerator.prototype.projectfiles = function projectfiles() {
-  this.mkdir('src');
-  this.mkdir('src/images');
-  this.mkdir('src/sprites');
-  this.mkdir('dist');
+  askForMetatags: function() {
+    var cb = this.async();
 
-  if (this.e2e || this.unit) {
-    this.mkdir('spec');
+    var prompts = [
+      {
+        type: 'confirm',
+        name: 'metatags',
+        message: 'metatags'
+      },
+      {
+        type: 'confirm',
+        name: 'analytics',
+        message: 'google analytics'
+      }
+    ];
 
-    if (this.e2e) {
-      this.mkdir('spec/e2e');
-      this.copy('spec/e2e/example.js', 'spec/e2e/example.js');
+    this.prompt(prompts, function(props) {
+      this.metatags = props.metatags;
+      this.analytics = props.analytics;
+
+      cb();
+    }.bind(this));
+  },
+
+  askForAngular: function() {
+    var cb = this.async();
+
+    var prompts = [];
+
+    if (this.type === 'web app') {
+      prompts = [
+        {
+          type: 'confirm',
+          name: 'angular',
+          message: 'angular'
+        },
+        {
+          type: 'confirm',
+          name: 'tests',
+          message: 'tests',
+          when: function(props) {
+            return props.angular;
+          }
+        },
+        {
+          type: 'confirm',
+          name: 'unit',
+          message: 'unit testing (jasmine)',
+          when: function(props) {
+            return props.tests;
+          }
+        },
+        {
+          type: 'confirm',
+          name: 'e2e',
+          message: 'e2e testing (casper.js)',
+          when: function(props) {
+            return props.tests;
+          }
+        }
+      ];
     }
 
-    if (this.unit) {
-      this.copy('karma.conf.js', 'karma.conf.js');
+    this.prompt(prompts, function(props) {
+      this.angular = props.angular;
+      this.tests = props.tests;
+      this.unit = props.unit;
+      this.e2e = props.e2e;
 
-      this.mkdir('spec/unit');
-      this.copy('spec/unit/exampleSpec.js', 'spec/unit/exampleSpec.js');
+      cb();
+    }.bind(this));
+  },
+
+  projectfiles: function() {
+    this.mkdir('src');
+    this.mkdir('src/images');
+    this.mkdir('src/sprites');
+    this.mkdir('dist');
+
+    if (this.e2e || this.unit) {
+      this.mkdir('spec');
+
+      if (this.e2e) {
+        this.mkdir('spec/e2e');
+        this.copy('spec/e2e/example.js', 'spec/e2e/example.js');
+      }
+
+      if (this.unit) {
+        this.copy('karma.conf.js', 'karma.conf.js');
+
+        this.mkdir('spec/unit');
+        this.copy('spec/unit/exampleSpec.js', 'spec/unit/exampleSpec.js');
+      }
+    }
+
+    this.copy('bowerrc', '.bowerrc');
+    this.copy('gitignore', '.gitignore');
+    this.copy('editorconfig', '.editorconfig');
+    this.copy('jshintrc', '.jshintrc');
+    this.copy('_bower.json', 'bower.json');
+    this.copy('gulpfile.js', 'gulpfile.js');
+    this.copy('gulpfile-production.js', 'gulpfile-production.js');
+    this.copy('substitute-config.js', 'substitute-config.js');
+    this.copy('_package.json', 'package.json');
+
+    this.template('src/index.html', 'src/index.html');
+
+    this.copy('src/styles/main.less', 'src/styles/main.less');
+    this.copy('src/scripts/main.js', 'src/scripts/main.js');
+
+    if (this.angular) {
+      this.copy('src/scripts/home/index.js', 'src/scripts/home/index.js');
+      this.copy('src/scripts/home/HomeCtrl.js', 'src/scripts/home/HomeCtrl.js');
     }
   }
-
-  this.copy('bowerrc', '.bowerrc');
-  this.copy('gitignore', '.gitignore');
-  this.copy('editorconfig', '.editorconfig');
-  this.copy('jshintrc', '.jshintrc');
-  this.copy('_bower.json', 'bower.json');
-  this.copy('gulpfile.js', 'gulpfile.js');
-  this.copy('gulpfile-production.js', 'gulpfile-production.js');
-  this.copy('substitute-config.js', 'substitute-config.js');
-  this.copy('_package.json', 'package.json');
-
-  this.template('src/index.html', 'src/index.html');
-
-  this.copy('src/styles/main.less', 'src/styles/main.less');
-  this.copy('src/scripts/main.js', 'src/scripts/main.js');
-
-  if (this.angular) {
-    this.copy('src/scripts/home/index.js', 'src/scripts/home/index.js');
-    this.copy('src/scripts/home/HomeCtrl.js', 'src/scripts/home/HomeCtrl.js');
-  }
-};
+});
