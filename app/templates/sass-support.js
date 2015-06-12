83361897ce2ef8e676872e089da8744d680ce55b
Pavel Gavlík <gav.pavel@gmail.com>
2015-06-12 17:03:45 +0200

    replace clean task with del npm module

--- app/templates/sass-support.js
+++ app/templates/sass-support.js
@@ -1,5 +1,6 @@
 // styles task override for Sass
 module.exports = function (pho) {
+  var del = require('del');
   var gulp = require('gulp');
   var path = require('path');
   var gulpSass = require('gulp-sass');
@@ -26,27 +27,25 @@ module.exports = function (pho) {
     var spriteFilter = $.filter('**/*.png');
     var cssFilter = $.filter('**/*.css');
 
-    gulp.src(path.join(config.dist.styleDir, config.dist.styleFiles), { read: false })
-      .pipe($.clean())
-      .on('end', function() {
-        gulp.src(path.join(config.src.styleDir, config.src.styleMain))
-          .pipe($.plumber(config.plumber))
-          .pipe(gulpSass(sassConfig))
-          .on('error', function () {
-            // continous error reporting hack
-            cb(null);
-          })
-          .pipe($.base64(config.base64))
+    del(path.join(config.dist.styleDir, config.dist.styleFiles), function () {
+      gulp.src(path.join(config.src.styleDir, config.src.styleMain))
+        .pipe($.plumber(config.plumber))
+        .pipe(gulpSass(sassConfig))
+        .on('error', function () {
+          // continous error reporting hack
+          cb(null);
+        })
+        .pipe($.base64(config.base64))
 
-          .pipe($.spritesPreprocessor(config.spritesPreprocessor))
-          .pipe(spriteFilter)
-          .pipe(gulp.dest(config.dist.spriteDir))
-          .pipe(spriteFilter.restore())
+        .pipe($.spritesPreprocessor(config.spritesPreprocessor))
+        .pipe(spriteFilter)
+        .pipe(gulp.dest(config.dist.spriteDir))
+        .pipe(spriteFilter.restore())
 
-          .pipe(cssFilter)
-          .pipe($.rename({ suffix: '-' + Date.now().toString() }))
-          .pipe(gulp.dest(config.dist.styleDir))
-          .on('end', cb);
-      });
+        .pipe(cssFilter)
+        .pipe($.rename({ suffix: '-' + Date.now().toString() }))
+        .pipe(gulp.dest(config.dist.styleDir))
+        .on('end', cb);
+    });
   });
 };
